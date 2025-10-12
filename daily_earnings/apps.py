68b11b17fcf9576efe310799b38541cab9aa37b2
Your Name <you@example.com>
2025-10-12 00:33:48 +0000

    feat: Initial project setup with Django and allauth
    - Initialize Django project 'puncha_mera'
    
    - Create several applications: home, time_stamp, time_entries, accounts, projects, reports, daily_earnings, income_calculator, invoices, analytics, schedule, retail, pmail
    
    - Configure static and template directories
    
    - Install and configure django-allauth for authentication

---
+++ daily_earnings/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DailyEarningsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'daily_earnings'
---
+++ puncha_mera/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for puncha_mera project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puncha_mera.settings')
+
+application = get_asgi_application()
