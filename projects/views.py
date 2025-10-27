593e6bfc87990f25022e6c7a2a0776fbf646c596
Your Name <you@example.com>
2025-10-27 16:51:54 +0000

    feat: Improve dark theme and fix archived tab
    This commit addresses several issues related to the dark theme and the functionality of the "archived" tab.
    
    The styling of the tabs in the dark theme has been improved to provide a better user experience. The selected tab now has a lighter grey background, and the font color is white on hover and when active. The border color of the tabs has been made consistent with the primary color of the application in both light and dark themes.
    
    The "archived" tab, which was previously not working, has been fixed.

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
 from .models import Project
 from .mixins import OrganizationPermissionMixin
 from accounts.models import Membership
@@ -6,6 +8,22 @@ from accounts.models import Membership
 class ProjectListView(OrganizationPermissionMixin, ListView):
     model = Project
     template_name = 'projects/project_list.html'
+
+    def get_queryset(self):
+        user = self.request.user
+        try:
+            membership = Membership.objects.get(user=user)
+            organization = membership.organization
+            return Project.objects.filter(organization=organization)
+        except Membership.DoesNotExist:
+            return Project.objects.none()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        queryset = self.get_queryset()
+        context['active_projects'] = queryset.filter(archived=False)
+        context['archived_projects'] = queryset.filter(archived=True)
+        return context
 
 class ProjectCreateView(OrganizationPermissionMixin, CreateView):
     model = Project
@@ -27,3 +45,31 @@ class ProjectCreateView(OrganizationPermissionMixin, CreateView):
 class ProjectDetailView(OrganizationPermissionMixin, DetailView):
     model = Project
     template_name = 'projects/project_detail.html'
+
+class ProjectUpdateView(OrganizationPermissionMixin, UpdateView):
+    model = Project
+    fields = ['name', 'contact', 'description']
+    template_name = 'projects/project_form.html'
+
+class ProjectDeleteView(OrganizationPermissionMixin, DeleteView):
+    model = Project
+    template_name = 'projects/project_confirm_delete.html'
+    success_url = reverse_lazy('projects:project-list')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.deleted = True
+        self.object.save()
+        return redirect(self.get_success_url())
+
+def archive_project(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    project.archived = True
+    project.save()
+    return redirect('projects:project-list')
+
+def unarchive_project(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    project.archived = False
+    project.save()
+    return redirect('projects:project-list')
