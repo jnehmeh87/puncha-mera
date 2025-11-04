44717d9a20d21130f1aad0ce8eca3b13b26bb7bf
Your Name <you@example.com>
2025-11-04 00:26:35 +0000

    feat: Redesign home page and navbar

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
-    template_name = "home/home.html"
+    def get_template_names(self):
+        if self.request.user.is_authenticated:
+            return ["home/home_logged_in.html"]
+        else:
+            return ["home/home_logged_out.html"]
